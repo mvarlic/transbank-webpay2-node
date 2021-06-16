99f42cedaea08131088284a003e5714faa0f9ceb
mvarlic <miguel.vargas@continuumhq.com>
2021-06-16 18:02:52 -0500

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express';
-import { WebPayController } from '../controller/webpay.controller';
+import path from "path";
+import { WebPayController } from './controller/webpay.controller';
 const app = express()
 const PORT : string|number = process.env.PORT || 4000;
 
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+app.use(express.static(__dirname + '/public'));
+/*
 app.get("/",(req, res) =>{
     res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+});*/
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
 app.get('/create', WebPayController.createTransaction);
