e587a6103565fd6061509865a61a59caa5554bba
mvarlic <miguel.vargas@continuumhq.com>
2021-06-16 17:38:34 -0500

    fix

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { WebPayController } from '../controller/webpay.controller';
 const app = express()
 const PORT : string|number = process.env.PORT || 4000;
 
-app.use("*",(req, res) =>{
+app.use("/",(req, res) =>{
     res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
+
+app.get('/create', WebPayController.createTransaction);
 
 app.listen(PORT,() => console.log(`hosting ${PORT}`));
